502da83eb5319338fa36c6b46e5cf75e83d34b77
ImenDerradji <72688155+ImenDerradji@users.noreply.github.com>
2020-11-14 19:30:30 +0100

    Add files via upload
    Added 2nd condition for empty fields

--- tp2/formulaire.js
+++ tp2/formulaire.js
@@ -1,8 +1,8 @@
 function validation(){
-  // ou faire une validation de tous les champs en une seule fois (Méthode ProfAW)
+  // Méthode ProfAW: faire une validation de tous les champs en une seule fois
   document.getElementById("error").innerHTML = "";
   for(let item of document.querySelectorAll('.inputForm')){
-    if(item.value.length < 5){
+    if(item.value.length < 5 & item.value !== ""){
       // recuperation du label
       var label =   document.querySelector("label[for="+item.id+"]").textContent;
       // ajout du message d'erreur
@@ -11,7 +11,16 @@ function validation(){
       document.getElementById("error").classList.add("display");
       document.getElementById("resultat").classList.remove("display")
     }
-    
+    else if (item.value == ""){
+      // recuperation du label
+      var label =   document.querySelector("label[for="+item.id+"]").textContent;
+      // ajout du message d'erreur
+      document.getElementById("error").innerHTML += ` ${label} La saisie de ce champ est obligatoire <br/>`;
+      
+      // ajout de la classe CSS "display"
+      document.getElementById("error").classList.add("display");
+        document.getElementById("resultat").classList.remove("display")
+    }
   }
   if(document.getElementById("error").innerHTML == ""){
     var fname = document.getElementById("fname").value

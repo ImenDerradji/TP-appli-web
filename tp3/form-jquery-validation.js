e9c2621f0e20fba790def61a12f7e95f878195f5
ImenDerradji <72688155+ImenDerradji@users.noreply.github.com>
2020-11-16 12:17:02 +0100

    Add files via upload
    Bootstrap Js

---
+++ tp3/form-jquery-validation.js
@@ -0,0 +1,10 @@
+//$( document ).ready(function() {
+    // ce code est exécuter une fois que toute la page est téléchargée par le navigateur
+    // voir plus : https://www.w3schools.com/js/js_htmldom.asp
+//     console.log( "DOM ready!" );
+//     $("#submit").on("click",function(event){
+//        event.preventDefault();
+//       console.log( "click" );
+//      });
+     // Y mettre le code jQuery pour valider tous les champs du formulaire
+// });

e20e59f62572ea3a47e28dd6eed30e4158e22c10
ImenDerradji <72688155+ImenDerradji@users.noreply.github.com>
2020-11-21 02:28:00 +0100

    Add files via upload
    Modal added + imgLink G.maps

--- tp3/form-jquery-validation.js
+++ tp3/form-jquery-validation.js
@@ -1,10 +1,18 @@
-//$( document ).ready(function() {
-    // ce code est exécuter une fois que toute la page est téléchargée par le navigateur
-    // voir plus : https://www.w3schools.com/js/js_htmldom.asp
-//     console.log( "DOM ready!" );
-//     $("#submit").on("click",function(event){
-//        event.preventDefault();
-//       console.log( "click" );
-//      });
-     // Y mettre le code jQuery pour valider tous les champs du formulaire
-// });
+$( document ).ready(function() {
+    $("#valider").on("click",function(event){
+        event.preventDefault();
+
+        if (($("#lname").val() && $("#fname").val() && $("#birthdate").val() && $("#address").val() && $("#email").val())!=""){
+            $(".modal-title").text("Bienvenue " + "" + $("#fname").val());
+            var city = $("#address").val();
+            $(".modal-body").html(`<a href="https://www.google.com/maps"> 
+            <img src="https://maps.googleapis.com/maps/api/staticmap?markers=${city}&zoom=14&size=400x300&scale=2&key=AIzaSyAkmvI9DazzG9p77IShsz_Di7-5Qn7zkcg">`);
+            $('#myModal').modal("show");
+        }   
+        else {
+            $(".modal-title").text("Erreur");
+            $(".modal-body").text("La saisie de tous les champs est obligatoire");
+            $('#myModal').modal("show");
+        }
+    });
+});

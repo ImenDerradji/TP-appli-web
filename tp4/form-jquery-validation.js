53bfc0b580f1b70e835c7ea519c5b74b422706d6
ImenDerradji <72688155+ImenDerradji@users.noreply.github.com>
2020-11-23 12:11:01 +0100

    Add files via upload
    TP4 - index.htm + css & js

---
+++ tp4/form-jquery-validation.js
@@ -0,0 +1,29 @@
+$( document ).ready(function() {
+    $("#valider").on("click",function(event){
+        event.preventDefault();
+
+        if (($("#lname").val() && $("#fname").val() && $("#birthdate").val() && $("#address").val() && $("#email").val())!=""){
+            $(".modal-title").text("Bienvenue " + "" + $("#fname").val());
+            var city = $("#address").val();
+            $(".modal-body").html(`<a href="https://www.google.com/maps"> 
+            <img src="https://maps.googleapis.com/maps/api/staticmap?markers=${city}&zoom=14&size=400x300&scale=2&key=AIzaSyAkmvI9DazzG9p77IShsz_Di7-5Qn7zkcg">`);
+            $('#myModal').modal("show");
+        }   
+        else {
+            $(".modal-title").text("Erreur");
+            $(".modal-body").text("La saisie de tous les champs est obligatoire");
+            $('#myModal').modal("show");
+        }
+    });
+
+    $("#gps").on("click",function(event){
+        event.preventDefault();
+        getLocation()
+        $("#lat").val(position.coords.latitude); 
+        $("#lng").val(position.coords.longitude);
+    });
+
+    $(".form-control").keyup(function(event) {
+        $("#car").text($(this).val().length + ' car.');
+    });
+});
